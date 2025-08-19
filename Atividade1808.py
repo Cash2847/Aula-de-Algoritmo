e1296d51149a9dddcb540e420ddfbe318a9fb8df
Cash2847 <sampaiolorenzo79@gmail.com>
2025-08-19 13:26:29 -0300

    Update Atividade1808.py

--- Atividade1808.py
+++ Atividade1808.py
@@ -18,3 +18,23 @@ print("O maior número é:", Maior_Número)
 
 
 # 4. Desenvolva um programa que leia uma lista de números e mostre a média deles.
+
+num1 = int(input("digite o número "))
+num2 = int(input("digite o número "))
+
+def Cálculo(num1, num2):
+    if num1 > num2:
+        return num1
+    elif num2 > num1:
+        return num2
+    else:
+        return None
+        
+Cálculo(num1, num2)
+
+resultado = Cálculo(num1, num2)
+
+if resultado == None:
+    print("São iguais.")
+else:
+    print(f"O número maior é", resultado)
