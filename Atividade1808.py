e7013f344b5bdddc6f307b13d46d7472e6c43267
Cash2847 <sampaiolorenzo79@gmail.com>
2025-08-18 21:51:50 -0300

    Add files via upload

---
+++ Atividade1808.py
@@ -0,0 +1,20 @@
+# 1. Faça um programa que leia dois números e mostre qual é o maior.
+
+X = float(input('Digite um número:' ))
+
+Y = float(input('Digite um número:' ))
+
+Maior_Número = max(X, Y)
+
+print("O maior número é:", Maior_Número)
+    
+
+# 2. Escreva um algoritmo que calcule a tabuada de um número digitado pelo usuário.
+
+
+
+# 3. Crie uma função que verifique se um número é primo.
+
+
+
+# 4. Desenvolva um programa que leia uma lista de números e mostre a média deles.
