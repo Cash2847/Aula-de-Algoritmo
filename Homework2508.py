30d6cb403a2fe20e954de44e88a3a8f56b2edbad
Cash2847 <sampaiolorenzo79@gmail.com>
2025-08-25 21:52:24 -0300

    Update Homework2508.py

---
+++ Homework2508.py
@@ -0,0 +1,76 @@
+# Caso1: Controle de Presença em Sala de Aula
+# Uma professora precisa registrar a presença dos alunos durante a semana.
+# • Cada dia da semana terá uma lista com os nomes dos presentes.
+# • No final, ela precisa:
+
+# 1. Saber quais alunos estiveram presentes todos os dias.
+# 2. Saber quais alunos faltaram em pelo menos um dia.
+# 3. Saber o número total de presenças por aluno.
+
+
+
+# Caso2: Distâncias em Km
+
+# 1. Receba as distâncias percorridas em 6 viagens e armazene em uma lista.
+# 2. Calcule a distância total percorrida.
+# 3. Mostre a maior e a menor distância.
+# 4. Calcule a média das distâncias arredondada para cima (use math.ceil).
+
+
+
+# Caso3: Supermercado – Controle de Estoque
+# Um supermercado mantém uma lista de produtos e seus preços.
+# • Cada item será representado como [nome, quantidade, preco_unitario].
+# • O sistema deve:
+
+# 1. Calcular o valor total em estoque.
+# 2. Encontrar o produto de maior valor total (quantidade × preço).
+# 3. Gerar uma lista apenas com os nomes dos produtos com estoque abaixo de 5
+# unidades.
+
+
+
+# 4. Permitir buscar um produto pelo nome e retornar seus dados.
+# Caso4: Análise de Vendas Mensais
+# Uma loja online registra o número de vendas de cada dia do mês em uma lista.
+# • Exemplo: [10, 15, 20, 5, 0, 8, ...]
+# O gerente precisa:
+
+# 1. Calcular o total de vendas no mês.
+# 2. Descobrir o dia com mais vendas e o dia com menos vendas.
+# 3. Calcular a média de vendas por dia.
+# 4. Listar os dias que tiveram vendas acima da média.
+
+
+
+# Caso5: Controle de Participação em um Evento
+# Os organizadores de um evento registraram os nomes dos participantes de cada atividade em
+# listas separadas.
+# • Exemplo:
+# o Palestra: ["Ana", "Carlos", "Marina"]
+# o Workshop: ["Carlos", "João", "Ana"]
+# o Mesa-redonda: ["Marina", "João", "Paula"]
+# Eles precisam:
+
+# 1. Saber quem participou de todas as atividades.
+# 2. Saber quem participou de apenas uma atividade.
+# 3. Gerar uma lista com todos os nomes únicos dos participantes.
+# 4. Contar quantos participantes distintos houve no evento.
+
+
+
+# Caso6: Sistema de Biblioteca
+# Uma biblioteca mantém uma lista de livros emprestados, onde cada item é representado por
+# [titulo, usuario, dias_emprestado].
+# Exemplo:
+# [
+#  ["Dom Casmurro", "Ana", 5],
+#  ["1984", "Carlos", 12],
+#  ["O Hobbit", "Marina", 3]
+# ]
+# O sistema precisa:
+
+# 1. Listar apenas os livros que estão emprestados há mais de 7 dias.
+# 2. Encontrar o livro emprestado há mais tempo.
+# 3. Gerar uma lista apenas com os nomes dos usuários que têm livros emprestados.
+# 4. Calcular a média de dias de empréstimo.
