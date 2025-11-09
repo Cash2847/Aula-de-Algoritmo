ef682b39576c310cfecc1bcb272146e01fa2e27d
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-09 20:34:27 -0300

    Update BubbleSort.py

--- Trabalho/BubbleSort.py
+++ Trabalho/BubbleSort.py
@@ -1,7 +1,14 @@
-def bubbleSort(arr): 
-    n = len(arr) 
+minha_lista = [10, 32, 54, 11, 5, 33, 66, 323, 5, 12]
+print(f"Lista original: {minha_lista}")
+
+def bubble_sort(lista): 
+    n = len(lista) 
     for i in range(n):
-        for j in range(0, n-i-1):
-                if arr[j] > arr[j+1] : 
-                        arr[j], arr[j+1] = arr[j+1], arr[j]
-    print(arr)
+        for j in range(0, n- i - 1):
+                if lista[j] > lista[j + 1] : 
+                        lista[j], lista[j + 1] = lista[j + 1], lista[j]
+    print(lista)
+
+lista_organizada = bubble_sort(minha_lista)
+
+print(f"Lista organizada: {lista_organizada}")
