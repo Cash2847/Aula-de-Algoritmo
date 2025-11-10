fb838bb195951bb457c4309bd5cf895127f7f188
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 19:18:44 -0300

    Update BubbleSort.py

--- Trabalho/BubbleSort.py
+++ Trabalho/BubbleSort.py
@@ -3,18 +3,17 @@ import tracemalloc
 import time
 
 tracemalloc.start()
-
-
-minha_lista = [random.randint(0, 100) for i in range(0, 20)]
-print(f"Lista original: {minha_lista}")
-
 inicio = time.time()
 
+minha_lista = [random.randint(0, 100) for i in range(0, 100)]
+print(f"Lista original: {minha_lista}")
+
+
 def bubble_sort(minha_lista):
-    for numero in range(len(minha_lista)-1, 0, -1): # Garante que cada item é comparado
+    for numero in range(len(minha_lista)-1, 0, -1): 
         for i in range(numero):
-            if minha_lista[i] > minha_lista[i+1]: # Compara o elemento atual com o próximo
-        # Troca os elementos se estiverem fora de ordem
+            if minha_lista[i] > minha_lista[i+1]: 
+        
                 temp = minha_lista[i]
                 minha_lista[i] = minha_lista[i+1]
                 minha_lista[i+1] = temp
@@ -22,7 +21,7 @@ def bubble_sort(minha_lista):
 
 
 lista_organizada = bubble_sort(minha_lista)
-print(f"Lista organizada: { bubble_sort(minha_lista)}")
+print(f"\nLista ordenada: { bubble_sort(minha_lista)}")
 
 final = time.time()
 
@@ -30,6 +29,6 @@ memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
 
 tracemalloc.stop()
 
-print(f"O tempo de execução é: {final-inicio:.3f} segundos")
-print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
-print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
+print(f"\nO tempo de execução é: {final-inicio:.3f} segundos.")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB.")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB.")
