503801c43f490bd5a7d02ee75e71b678bd010b73
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:42:11 -0300

    Update BucketSort.py

--- Trabalho/BucketSort.py
+++ Trabalho/BucketSort.py
@@ -1,6 +1,14 @@
+import tracemalloc
+import time
+
+
+tracemalloc.start()
+
+
 minha_lista = [.22, .32, .43, .21, .20, .23, .25, .44, .20, .6763]
 print(f"Lista original: {minha_lista}")
 
+inicio = time.time()
 
 def bucket_sort(lista):
     buckets = []
@@ -23,3 +31,16 @@ def bucket_sort(lista):
 
 lista_organizada = bucket_sort(minha_lista)
 print(f"Lista organizada: {lista_organizada}")
+
+final = time.time()
+memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
+
+lista_organizada = bucket_sort(minha_lista)
+print(f"Lista organizada: {lista_organizada}")
+
