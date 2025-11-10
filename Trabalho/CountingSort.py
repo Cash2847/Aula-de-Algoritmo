a7c09f5447ed63bd62c0fbb53e0df2cc5a6cff99
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:42:50 -0300

    Update CountingSort.py

--- Trabalho/CountingSort.py
+++ Trabalho/CountingSort.py
@@ -1,6 +1,15 @@
 
-minha_lista = [4, 2, 2, 6, 3, 3, 1, 6, 5, 2, 3]
+import tracemalloc
+import time
+import random
+
+tracemalloc.start()
+
+
+minha_lista = [12, 43, 656, 23, 12, 11, 545, 66, 77, 9, 100, 33, 22, 44, 66, 564]
 print(f"Lista original: {minha_lista}")
+
+inicio = time.time()
 
 def counting_sort(lista):
   max_val = max(lista)
@@ -20,3 +29,14 @@ def counting_sort(lista):
 
 lista_organizada = counting_sort(minha_lista)
 print(f"Lista organizada: {lista_organizada}")
+
+final = time.time()
+
+memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
+
