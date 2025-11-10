81ba06c074d8a0eb4009fabadf2d27359e3b0723
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 20:23:20 -0300

    Update BubbleSort.py

--- Trabalho/BubbleSort.py
+++ Trabalho/BubbleSort.py
@@ -1,3 +1,12 @@
+# O Bubble Sort é uma função simples que, quando ordenando uma lista, pega um valor por vez, e o compara com o próximo número na lista.
+# Caso seja maior, o valor escolhido irá trocar de lugar com o número comparado e repetira o mesmo com o próximo até encontrar um número
+# maior do que o valor escolhido.
+
+# O Bubble Sort realiza essa mesma ação com todos os números da lista mostrada para assim, organiza-la por tamanho. Isso faz com que
+# essa função seja relativamente simples em seu funcionamento, e que a estrutura de seu código seja menos
+# complexa e extensa em comparação à outras opções. Porém, por esses mesmos motivos, esse método não apenas possui uma desvantagem
+# Quando a lista é muito grande, como também irá usar muita memória por comparar a lista de número a número.
+
 import random
 import tracemalloc
 import time
@@ -32,3 +41,4 @@ tracemalloc.stop()
 print(f"\nO tempo de execução é: {final-inicio:.3f} segundos.")
 print(f"A memória atual é: {memoria_atual/1024:.3f} KB.")
 print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB.")
+
