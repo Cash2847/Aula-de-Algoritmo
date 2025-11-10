0d3ec9a68915643d48f16c07ed9ec62944d970f0
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:44:06 -0300

    Update MergeSort.py

--- Trabalho/MergeSort.py
+++ Trabalho/MergeSort.py
@@ -1,5 +1,14 @@
-minha_lista = [3, 7, 6, -10, 15, 23.5, 55, -13]
+import tracemalloc
+import time
+import random
+
+tracemalloc.start()
+
+
+minha_lista = [random.randint(-100, 100) for i in range(0, 20)]
 print(f"Lista original: {minha_lista}")
+
+inicio = time.time()
 
 def merge(esquerda, direita):
   resultado = []
@@ -18,21 +27,31 @@ def merge(esquerda, direita):
 
   return resultado
 
-def merge_sort(arr):
+def merge_sort(minha_lista):
   step = 1
-  length = len(arr)
+  length = len(minha_lista)
 
   while step < length:
     for i in range(0, length, 2 * step):
-      left = arr[i:i + step]
-      right = arr[i + step:i + 2 * step]
+      left = minha_lista[i:i + step]
+      right = minha_lista[i + step:i + 2 * step]
       merged = merge(left, right)
       for j, val in enumerate(merged):
-        arr[i + j] = val
+        minha_lista[i + j] = val
     step *= 2
 
-  return arr
+  return minha_lista
 
 
 lista_organizada = merge_sort(minha_lista)
-print(f"Lista organizada: {lista_organizada}")
+print(f"Lista ordenada: {lista_organizada}")
+
+final = time.time()
+
+memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
