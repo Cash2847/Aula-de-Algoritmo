0867c3c52f411d7a556b141e0a3d74cd5cfc0b47
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:45:33 -0300

    Update SelectionSort.py

--- Trabalho/SelectionSort.py
+++ Trabalho/SelectionSort.py
@@ -1,8 +1,14 @@
 import tracemalloc
+import time
+import random
 
 tracemalloc.start()
+inicio = time.time()
 
-minha_lista = [12, 5434, 65, 23, 12, 342, 10, 3223]
+
+minha_lista = [random.randint(0, 100) for i in range(0, 20)]
+print(f"Lista original: {minha_lista}")
+
 
 n = len(minha_lista)
 for i in range(n - 1):
@@ -13,12 +19,16 @@ for i in range(n - 1):
     valor_minimo = minha_lista.pop(min_index)
     minha_lista.insert(i, valor_minimo)
 
-print(minha_lista)
+print(f"Lista ordenada: {minha_lista}")
+
+final = time.time()
 
 memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
 
 tracemalloc.stop()
 
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
 print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
 print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
+
 
