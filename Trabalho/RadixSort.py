07a17db0cafc1a130f71114f85ea412cc4e3c3bb
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:44:53 -0300

    Update RadixSort.py

--- Trabalho/RadixSort.py
+++ Trabalho/RadixSort.py
@@ -1,22 +1,39 @@
+import tracemalloc
+import time
+import random
 
-minha_lista = [170, 45, 75, 90, 802, 24, 2, 66]
+tracemalloc.start() 
+
+inicio = time.time()
+
+minha_lista = [random.randint(0, 100) for i in range(0, 20)]
 print(f"Lista original: {minha_lista}")
 lista_radix = [[], [], [], [], [], [], [], [], [], []]
-maxVal = max(minha_lista)
+valor_maximo = max(minha_lista)
 exp = 1
 
-while maxVal // exp > 0:
+while valor_maximo // exp > 0:
 
   while len(minha_lista) > 0:
-    val = minha_lista.pop()
-    indice_radix = (val // exp) % 10
-    lista_radix[indice_radix].append(val)
+    valor = minha_lista.pop()
+    indice_radix = (valor // exp) % 10
+    lista_radix[indice_radix].append(valor)
 
   for bucket in lista_radix:
     while len(bucket) > 0:
-        val = bucket.pop()
-        minha_lista.append(val)
+      valor = bucket.pop()
+      minha_lista.append(valor)
 
-    exp *= 10
+  exp *= 10
 
-print(f"Lista atualizada: {minha_lista}")
+print(f"Lista ordenada: {minha_lista}")
+
+final = time.time()
+
+memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
+
+tracemalloc.stop()
+
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
