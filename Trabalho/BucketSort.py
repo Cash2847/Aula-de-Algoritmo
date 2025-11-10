1a1f5e313544b473973f0229a8cb4a89a4066396
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 19:19:54 -0300

    Update BucketSort.py

--- Trabalho/BucketSort.py
+++ Trabalho/BucketSort.py
@@ -3,12 +3,10 @@ import time
 
 
 tracemalloc.start()
-
-
-minha_lista = [.22, .32, .43, .21, .20, .23, .25, .44, .20, .6763]
-print(f"Lista original: {minha_lista}")
-
 inicio = time.time()
+
+minha_lista = [.22, .32, .43, .21, .20, .23, .25, .44, .20, .6763, .122, .222, .12211, .21222, .1211, .55]
+print(f"Lista original: {minha_lista}")
 
 def bucket_sort(lista):
     buckets = []
@@ -30,17 +28,15 @@ def bucket_sort(lista):
     return lista
 
 lista_organizada = bucket_sort(minha_lista)
-print(f"Lista organizada: {lista_organizada}")
+print(f"\nLista ordenada: {lista_organizada}")
 
 final = time.time()
 memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
 
 tracemalloc.stop()
 
-print(f"O tempo de execução é: {final-inicio:.3f} segundos")
-print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
-print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
+print(f"\nO tempo de execução é: {final-inicio:.3f} segundos.")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB.")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB.")
 
-lista_organizada = bucket_sort(minha_lista)
-print(f"Lista organizada: {lista_organizada}")
 
