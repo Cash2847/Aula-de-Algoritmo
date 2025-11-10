d23720db5e652e9bc57fc960cb35afe6b0fc8b9f
Cash2847 <sampaiolorenzo79@gmail.com>
2025-11-10 16:43:46 -0300

    Update InsertionSort.py

--- Trabalho/InsertionSort.py
+++ Trabalho/InsertionSort.py
@@ -1,5 +1,13 @@
-minha_lista = [64, 34, 25, 5, 22, 11, 90, 12]
+import tracemalloc
+import time
+import random
+
+tracemalloc.start()
+
+minha_lista = [random.randint(0, 100) for i in range(0, 20)]
 print(f"Lista original: {minha_lista}")
+
+inicio = time.time()
 
 n = len(minha_lista)
 for i in range(n-1):
@@ -11,3 +19,12 @@ for i in range(n-1):
   minha_lista.insert(i, min_value)
 
 print(f"Lista organizada: {minha_lista}") 
+
+final = time.time()
+
+memoria_atual,memoria_pico = tracemalloc.get_traced_memory()
+tracemalloc.stop()
+
+print(f"O tempo de execução é: {final-inicio:.3f} segundos")
+print(f"A memória atual é: {memoria_atual/1024:.3f} KB")
+print(f"A memória em seu pico é: {memoria_pico/1024:.3f} KB")
